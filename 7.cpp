93d35dd1887f01507b95a687ad40c8c24f533b0d
MuhammadTaha01 <127325759+MuhammadTaha01@users.noreply.github.com>
2023-03-11 01:01:39 +0500

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float cel, faren;
+    cout<<"Enter temperature in celsius= ";
+    cin>>cel;
+    faren= 9.0/5.0 * cel + 32;
+    cout<<"Temperature in Farhenheight is= ";
+    cout<<faren;
+    return 0;
+}
